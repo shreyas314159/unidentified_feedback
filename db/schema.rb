7814d5c7d4eca76a93808cdd9e7c13d39f91a679
Shreyas Prakash <shreyas314159@gmail.com>
2023-08-11 13:08:35 +0530

    First commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_11_072549) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.string "text"
+    t.string "recipient_handle"
+    t.string "tweet_url"
+    t.string "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "finds", force: :cascade do |t|
+    t.string "text"
+    t.string "text_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inputs", force: :cascade do |t|
+    t.string "text"
+    t.string "text_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
