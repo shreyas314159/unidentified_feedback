484d3072668d44a3e3c0735395264585b0a37321
Shreyas Prakash <shreyas314159@gmail.com>
2023-08-11 14:14:29 +0530

    setting up basic frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/exit'
+  get 'pages/home'
+  get 'pages/terms'
+  get 'pages/privacy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
+ 
 end
